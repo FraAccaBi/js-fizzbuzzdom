1664054cd2bce54883724613ba08c5dbf013a7a4
FraAccaBi <francescahuda@gmail.com>
2022-03-28 15:04:26 +0200

    complete if cicle

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,13 @@
 
 
 for (let i = 1; i <=100; i++) {
-    console.log(i);
-    if ( (i % 3) == 0) {
-       console.log('Fizz');
+    if (((i % 5) == 0) && ((i % 3) == 0)) {
+        console.log('BuzzFizz');
+    } else if ( (i % 3) == 0) {
+        console.log('Fizz');
+    } else if ( (i % 5) == 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
     }
 }
