9013ce404ff1a74e3ab186ef905567832e9c18f0
FraAccaBi <francescahuda@gmail.com>
2022-03-28 15:39:45 +0200

    BONUS add classes to html list

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,21 @@ const listHTML = document.getElementById('lista')
 
 for (let i = 1; i <=100; i++) {
     if (((i % 5) == 0) && ((i % 3) == 0)) {
-        let listItem_BuzzFizz = `<li>BuzzFizz</li>`
+        let listItem_BuzzFizz = `<div class="list-item-BuzzFizz">BuzzFizz</div>`
         listHTML.innerHTML += listItem_BuzzFizz
         console.log('BuzzFizz');
     } else if ( (i % 3) == 0) {
-        let listItem_Fizz = document.createElement("li")
+        let listItem_Fizz = document.createElement("div")
+        listItem_Fizz.classList.add("list-item-Fizz");
         listItem_Fizz.innerHTML = 'Fizz';
         listHTML.appendChild(listItem_Fizz);
         console.log('Fizz');
     } else if ( (i % 5) == 0) {
-        let listItem_Buzz = `<li>Buzz</li>`
+        let listItem_Buzz = `<div class="list-item-Buzz">Buzz</div>`
         listHTML.innerHTML += listItem_Buzz;
         console.log('Buzz');
     } else {
-        let listItem = `<li>${i}</li>`;
+        let listItem = `<div class="list-item">${i}</div>`;
         listHTML.innerHTML += listItem; 
         console.log(i);
     }
@@ -25,11 +26,3 @@ for (let i = 1; i <=100; i++) {
 }
 
 
-/* for (let i= 1000; i >= 500; i--){
-    console.log(i);
-
-    let listItem = document.createElement('li' )
-    listItem.append(`n° ${i}`) //inserisce nel li il contenuto 
-    listHTML.append(listItem) //aggiunge li alla ul
-    listItem.classList.add(`item-${i}`) //aggiunge la classe con classlist
-} */
