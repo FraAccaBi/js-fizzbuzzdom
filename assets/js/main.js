fd3b176b559fd78c89f8a5dde2f6ad480a7c06f2
FraAccaBi <francescahuda@gmail.com>
2022-03-28 15:53:18 +0200

    BONUS add BS link and col structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,23 @@ const listHTML = document.getElementById('lista')
 
 for (let i = 1; i <=100; i++) {
     if (((i % 5) == 0) && ((i % 3) == 0)) {
-        let listItem_BuzzFizz = `<div class="list-item-BuzzFizz">BuzzFizz</div>`
+        let listItem_BuzzFizz = `<div class="list-item-BuzzFizz col">BuzzFizz</div>`;
         listHTML.innerHTML += listItem_BuzzFizz
         console.log('BuzzFizz');
     } else if ( (i % 3) == 0) {
         let listItem_Fizz = document.createElement("div")
-        listItem_Fizz.classList.add("list-item-Fizz");
+        listItem_Fizz.classList.add("list-item-Fizz")
+        listItem_Fizz.classList.add("col");
         listItem_Fizz.innerHTML = 'Fizz';
         listHTML.appendChild(listItem_Fizz);
         console.log('Fizz');
     } else if ( (i % 5) == 0) {
-        let listItem_Buzz = `<div class="list-item-Buzz">Buzz</div>`
+        let listItem_Buzz = `<div class="list-item-Buzz col">Buzz</div>`
         listHTML.innerHTML += listItem_Buzz;
         console.log('Buzz');
     } else {
-        let listItem = `<div class="list-item">${i}</div>`;
-        listHTML.innerHTML += listItem; 
+        let listItem = `<div class="list-item col">${i}</div>`;
+        listHTML.innerHTML += listItem;
         console.log(i);
     }
 
